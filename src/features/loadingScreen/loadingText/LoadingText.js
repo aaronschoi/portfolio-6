8467a7bb69c7a74a147b081c9a2cebd95e2144fe
AaronChoi <aaronschoilcw@gmail.com>
2021-12-10 17:28:52 -0800

    loading screen looking good :D

--- src/features/loadingScreen/loadingText/LoadingText.js
+++ src/features/loadingScreen/loadingText/LoadingText.js
@@ -1,9 +1,20 @@
+import { changeToDesktop } from "../../../store/currentWindowStore"
 import "./loadingText.module.css"
 
 const LoadingText = () => {
+
+    React.useEffect(() => {
+        const timeLine = gsap.timeline({onComplete: changeToDesktop})
+        timeLine.to("#loading-text", {duration: 3, text: "for the most immersive experience, press F11", delay: 3})
+        .to("#loading-text", {duration: 3, text: "thank you for visiting my portfolio!", delay: 3})
+        .to("#loading-text", {duration: 3})
+
+        return () => timeLine.kill()
+    }, [])
+
     return (
         <div className="loading-text-container">
-            <div>start scrolling</div>
+            <div id="loading-text">loading</div>
         </div>
     )
 }
