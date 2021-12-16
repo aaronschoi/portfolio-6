7430e2f02915830c3894b7b79803b0c054b004ca
AaronChoi <aaronschoilcw@gmail.com>
2021-12-16 08:46:30 -0800

    just in case

--- src/features/BSCode/container/explorerbar/BSCodeExplorerBar.js
+++ src/features/BSCode/container/explorerbar/BSCodeExplorerBar.js
@@ -1,39 +1,44 @@
-import ExplorerComponent from "./explorerComponent/ExplorerComponent"
+import ExplorerComponent from "./explorerComponent/ExplorerComponent";
+import "./bscodeExplorerBar.module.css";
 
 const BSCodeExplorerBar = () => {
-    //it's actually 5 of the same components with different data in each
-    return (
-        <div className="bscode-explorer-container">
-            <div className="bscode-explorer-options">
-                <div>EXPLORER</div>
-                <div>...</div>
-            </div>
-            <ExplorerComponent title="OPEN EDITORS">
-                <div>someFile.js</div>
-                <div>someOtherFile.js</div>
-                <div>yetAnotherFile.js</div>
-            </ExplorerComponent>
-            <ExplorerComponent title="FRONTEND">
-                <div>someFile.js</div>
-                <div>someOtherFile.js</div>
-                <div>yetAnotherFile.js</div>
-            </ExplorerComponent>
-            <ExplorerComponent title="OUTLINE">
-                <div>someFile.js</div>
-                <div>someOtherFile.js</div>
-                <div>yetAnotherFile.js</div>
-            </ExplorerComponent>
-            <ExplorerComponent title="TIMELINE">
-                <div>Uncommited Changes</div>
-            </ExplorerComponent>
-            <ExplorerComponent title="NPM SCRIPTS">
-                <div>dev</div>
-                <div>build</div>
-                <div>serve</div>
-                <div>start</div>
-            </ExplorerComponent>
-        </div>
-    )
-}
+	//it's actually 5 of the same components with different data in each
+	return (
+		<div className="bscode-explorer-container">
+			<div className="bscode-explorer-options">
+				<div>EXPLORER</div>
+				<div>...</div>
+			</div>
+			<ExplorerComponent title="OPEN EDITORS">
+				<ExplorerComponent title="someFile.js" icon="JS" />
+				<ExplorerComponent title="someOtherFile.js" icon="JS" />
+				<ExplorerComponent title="yetAnotherFile.js" icon="JS" />
+			</ExplorerComponent>
+			<ExplorerComponent title="FRONTEND">
+				<ExplorerComponent title="folder" spacer={1}>
+					<ExplorerComponent title="folderWithin" spacer={1}>
+						<ExplorerComponent title="someJSFile.js" icon="JS" />
+					</ExplorerComponent>
+				</ExplorerComponent>
+				<ExplorerComponent title="someOtherFile.js" icon="JS" />
+				<ExplorerComponent title="yetAnotherFile.js" icon="JS" />
+			</ExplorerComponent>
+			<ExplorerComponent title="OUTLINE">
+				<div>someFile.js</div>
+				<div>someOtherFile.js</div>
+				<div>yetAnotherFile.js</div>
+			</ExplorerComponent>
+			<ExplorerComponent title="TIMELINE">
+				<div>Uncommited Changes</div>
+			</ExplorerComponent>
+			<ExplorerComponent title="NPM SCRIPTS">
+				<div>dev</div>
+				<div>build</div>
+				<div>serve</div>
+				<div>start</div>
+			</ExplorerComponent>
+		</div>
+	);
+};
 
 export default BSCodeExplorerBar;
--- src/features/BSCode/container/explorerbar/explorerComponent/ExplorerComponent.js
+++ src/features/BSCode/container/explorerbar/explorerComponent/ExplorerComponent.js
@@ -1,16 +1,27 @@
 import "./bscodeExplorerComponent.module.css";
 
-const ExplorerComponent = ({ title, children }) => {
+const ExplorerComponent = ({ title, icon = "", children }) => {
 	const [arrow, setArrow] = React.useState(false);
 	const handleExpand = (event) => {
-		setArrow((current) => !current);
+		if (icon === "") {
+			setArrow((current) => !current);
+		}
 	};
-	const caret = <div>{arrow ? ">" : "v"}</div>;
-	const fileView = arrow ? <div>{children}</div> : null;
+	const caret =
+		icon === "" ? <div>{arrow ? ">" : "v"}</div> : <div>{icon}</div>;
+	const fileView = arrow ? (
+		<div className="bscode-explorer-expander">{children}</div>
+	) : null;
+	const tab = { transform: `translate(${icon === "" ? "1" : ""}0px)` };
 	return (
-		<div>
-			<div onClick={handleExpand} className="bscode-explorer-component-container" tabIndex="0">
-				{caret} {title}
+		<div style={tab}>
+			<div
+				onClick={handleExpand}
+				className="bscode-explorer-component-container"
+				tabIndex="0"
+			>
+				{caret}
+				{title}
 			</div>
 			{fileView}
 		</div>
