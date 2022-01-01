fa79f5fde8677cad66ca88668cc20ce8fb13dab8
AaronChoi <aaronschoilcw@gmail.com>
2022-01-01 13:48:04 -0800

    terminal topbar updates

--- src/features/BSCode/mainContent/terminal/TerminalTopBar/TerminalTopBar.js
+++ src/features/BSCode/mainContent/terminal/TerminalTopBar/TerminalTopBar.js
@@ -1,22 +1,24 @@
+import "./terminaltopbar.module.css";
+
 const TerminalTopBar = () => {
 	return (
-		<div>
-			<div>
-				<div>TERMINAL</div>
-				<div>PROBLEMS</div>
-				<div>OUTPUT</div>
-				<div>DEBUG CONSOLE</div>
+		<div className="terminal-topbar-container">
+			<div className="terminal-topbar-left-options">
+				<div className="terminal-topbar-picked">TERMINAL</div>
+				<div className="terminal-topbar-left-option">PROBLEMS</div>
+				<div className="terminal-topbar-left-option">OUTPUT</div>
+				<div className="terminal-topbar-left-option">DEBUG CONSOLE</div>
 			</div>
-            <div>
-                <div>box</div>
-                <div>bash</div>
-                <div>plus</div>
-                <div>down chev</div>
-                <div>double</div>
-                <div>trash</div>
-                <div>up chev</div>
-                <div>x</div>
-            </div>
+			<div className="terminal-topbar-right-options">
+				<div>box</div>
+				<div>bash</div>
+				<div>plus</div>
+				<div>down chev</div>
+				<div>double</div>
+				<div>trash</div>
+				<div>up chev</div>
+				<div>x</div>
+			</div>
 		</div>
 	);
 };
