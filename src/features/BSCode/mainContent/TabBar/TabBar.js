9c60bc51f0f94ebafb4925dcc3cef69518d36e9e
AaronChoi <aaronschoilcw@gmail.com>
2022-01-01 14:40:51 -0800

    reverted one minor tabbar change

--- src/features/BSCode/mainContent/TabBar/TabBar.js
+++ src/features/BSCode/mainContent/TabBar/TabBar.js
@@ -3,14 +3,14 @@ import "./tabbar.module.css";
 import Tabs from "./Tabs/Tabs";
 
 const TabBar = () => {
-    return (
-        <div className="tabbar-container">
-            <Tabs />
-            <div>
-                <ThreeDots />
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="tabbar-container">
+			<Tabs />
+			<div>
+				<ThreeDots />
+			</div>
+		</div>
+	);
+};
 
 export default TabBar;
